e109214356c22e26e10b51bbf6e6d48c7f268b21
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-10-13 11:53:35 +0200

    Refactor cookie consent code

--- HoldYourHorses/wwwroot/JS/cookies.js
+++ HoldYourHorses/wwwroot/JS/cookies.js
@@ -1,20 +1,11 @@
-﻿window.onload = function () {
-    if (document.cookie.indexOf('.AspNet.Consent') == -1) {
-            var popup = document.getElementById("popup");
-            var overlay = document.getElementById("overlay");
-            overlay.style.display = "block";
-            popup.style.display = "block";
-    }
-    try {
-        document.getElementById("cookiebutton").onclick = function () {
-            var popup = document.getElementById("popup");
-            var overlay = document.getElementById("overlay");
-            overlay.style.display = "none";
-            popup.style.display = "none";
-        };
-    }
-    catch (error) { }
-        
-    };
+﻿const button = document.getElementById("cookie-button");
 
+console.log(button)
 
+// Closes the modal and sets the consent cookie when the user is pressing the button
+button.addEventListener("click", () => {
+    console.log(document.getElementById("cookie-dialog"))
+    document.cookie = button.dataset.cookieString;
+    document.getElementById("cookie-dialog").close();
+    document.getElementById("custom-backdrop").style.display = "none";
+});
