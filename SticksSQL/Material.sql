261da89f2a73b4f1f68376a0f3dedfc05a52da2c
Yousif Ali <107153597+YousifAli95@users.noreply.github.com>
2023-10-17 10:52:56 +0200

    Delete SticksSQL directory

--- SticksSQL/Material.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Material]
-(
-	[Id] INT NOT NULL PRIMARY KEY identity unique, 
-    [Namn] NVARCHAR(50) NOT NULL unique
-)
--- SticksSQL/Tillverkningsländer.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].OriginCountry
-(
-	[Id] INT NOT NULL PRIMARY KEY identity, 
-    [Namn] NVARCHAR(50) NOT NULL unique
-)
