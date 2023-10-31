17ebe8546a51cd6b8441e1ce0fb6086f25066804
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-10-31 12:21:20 +0100

    Refactor Index javascript code

--- HoldYourHorses/wwwroot/JS/index.js
+++ HoldYourHorses/wwwroot/JS/index.js
@@ -1,3 +1,5 @@
+////// Variables //////
+
 const fromSlider = document.querySelector("#fromSlider");
 const toSlider = document.querySelector("#toSlider");
 const fromInput = document.querySelector("#fromInput");
@@ -27,7 +29,7 @@ var types = "-";
 var materials = "-";
 var numberOfCompares = 0;
 
-// EventListeners
+////// EventListeners //////
 allTypes.forEach((o) => {
     types += " " + o.value;
     o.addEventListener("change", function () {
@@ -40,6 +42,7 @@ allTypes.forEach((o) => {
         }
     });
 });
+
 allMaterials.forEach((o) => {
     materials += " " + o.value;
     o.addEventListener("change", function () {
@@ -67,49 +70,8 @@ selectElement.addEventListener("change", (event) => {
     getPartialView();
 });
 
-//price  event listener
-fromSlider.addEventListener("change", (event) => {
-    minPrice = event.target.value;
-    getPartialView();
-});
 
-toSlider.addEventListener("change", (event) => {
-    maxPrice = event.target.value;
-    getPartialView();
-});
-
-fromInput.addEventListener("change", (event) => {
-    minPrice = event.target.value;
-    getPartialView();
-});
-
-toInput.addEventListener("change", (event) => {
-    maxPrice = event.target.value;
-    getPartialView();
-});
-
-//Hästkrafter event listener
-fromSliderHK.addEventListener("change", (event) => {
-    minHK = event.target.value;
-    getPartialView();
-});
-
-toSliderHK.addEventListener("change", (event) => {
-    maxHK = event.target.value;
-    getPartialView();
-});
-
-fromInputHK.addEventListener("change", (event) => {
-    minHK = event.target.value;
-    getPartialView();
-});
-
-toInputHK.addEventListener("change", (event) => {
-    maxHK = event.target.value;
-    getPartialView();
-});
-
-//functions
+////// Functions /////
 async function getPartialView() {
     const superContainer = document.querySelector(".card-container");
     const url = `/IndexPartial/?maxPrice=${maxPrice}&minPrice=${minPrice}&maxHK=${maxHK}&minHK=${minHK}&typer=${types}&materials=${materials}&sortOn=${sortOn}&isAscending=${isAscending}`;
@@ -245,9 +207,9 @@ function showHideFilter() {
         }
         else {
             setTimeout(
-                function (a) {
-                    a.style.display = "block";
-                },
+                () =>
+                    a.style.display = "block"
+                ,
                 300,
                 listItems[i]
             );
@@ -256,6 +218,7 @@ function showHideFilter() {
 
     isShown = !isShown;
     filterElement.classList.toggle(FILTER_CLOSE_CLASS);
+
     if (isShown) {
         hamburgerMenuSVG.style.transform = "";
         mainElement.classList.add(MAIN_DIV_CLOSED);
@@ -285,6 +248,7 @@ async function removeCompare() {
 function ShowOrHideCompareButton() {
     var button = document.getElementById("compare-btn");
     console.log(numberOfCompares);
+
     if (numberOfCompares < 2) {
         button.style.display = "none";
     } else {
@@ -295,6 +259,7 @@ function ShowOrHideCompareButton() {
 async function addHeart(svg, artikelNr) {
     let didAddHeart;
     const url = `/api/add-or-remove-favourite/${artikelNr}`
+
     await fetch(url, { method: "GET" })
         .then((o) => o.json())
         .then((o) => (didAddHeart = o.added));
@@ -309,6 +274,7 @@ async function addHeart(svg, artikelNr) {
 
 async function getHearts() {
     const fetchedArticleNumbers = await fetch(`/api/favourites`);
+
     try {
         articleNumbers = await fetchedArticleNumbers.json();
         console.log(articleNumbers);
@@ -318,107 +284,12 @@ async function getHearts() {
                 svg.classList.add(HEART_FILL_CLASS);
             }
         }
+
     } catch (error) { }
 }
 
-///// Slider Javascript code /////
-function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
-    const [from, to] = getParsed(fromInput, toInput);
-    fillSlider(fromInput, toInput, "black", "#7b63ad", controlSlider);
-    if (from > to) {
-        fromSlider.value = to;
-        fromInput.value = to;
-    } else {
-        fromSlider.value = from;
-    }
-}
-
-function controlToInput(toSlider, fromInput, toInput, controlSlider) {
-    const [from, to] = getParsed(fromInput, toInput);
-    fillSlider(fromInput, toInput, "black", "#7b63ad", controlSlider);
-    setToggleAccessible(toInput);
-    if (from <= to) {
-        toSlider.value = to;
-        toInput.value = to;
-    } else {
-        toInput.value = from;
-    }
-}
-
-function controlFromSlider(fromSlider, toSlider, fromInput) {
-    const [from, to] = getParsed(fromSlider, toSlider);
-    fillSlider(fromSlider, toSlider, "black", "#7b63ad", toSlider);
-    if (from > to) {
-        fromSlider.value = to;
-        fromInput.value = to;
-    } else {
-        fromInput.value = from;
-    }
-}
-
-function controlToSlider(fromSlider, toSlider, toInput) {
-    const [from, to] = getParsed(fromSlider, toSlider);
-    fillSlider(fromSlider, toSlider, "black", "#7b63ad", toSlider);
-    setToggleAccessible(toSlider);
-    if (from <= to) {
-        toSlider.value = to;
-        toInput.value = to;
-    } else {
-        toInput.value = from;
-        toSlider.value = from;
-    }
-}
-
-function getParsed(currentFrom, currentTo) {
-    const from = parseInt(currentFrom.value, 10);
-    const to = parseInt(currentTo.value, 10);
-    return [from, to];
-}
-
-function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-    const rangeDistance = to.max - to.min;
-    const fromPosition = from.value - to.min;
-    const toPosition = to.value - to.min;
-    controlSlider.style.background = `linear-gradient(
-      to right,
-      ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
-      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
-      ${sliderColor} 100%)`;
-}
-
-function setToggleAccessible(currentTarget) {
-    const toSlider = document.querySelector("#toSlider");
-    if (Number(currentTarget.value) <= 0) {
-        toSlider.style.zIndex = 2;
-    } else {
-        toSlider.style.zIndex = 0;
-    }
-}
-
-fillSlider(fromSlider, toSlider, "black", "#7b63ad", toSlider);
-setToggleAccessible(toSlider);
-
-fillSlider(fromSliderHK, toSliderHK, "black", "#7b63ad", toSliderHK);
-setToggleAccessible(toSliderHK);
-
-fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
-toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
-fromInput.oninput = () =>
-    controlFromInput(fromSlider, fromInput, toInput, toSlider);
-toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
-
-fromSliderHK.oninput = () =>
-    controlFromSlider(fromSliderHK, toSliderHK, fromInputHK);
-toSliderHK.oninput = () => controlToSlider(fromSliderHK, toSliderHK, toInputHK);
-fromInputHK.oninput = () =>
-    controlFromInput(fromSliderHK, fromInputHK, toInputHK, toSliderHK);
-toInputHK.oninput = () =>
-    controlToInput(toSliderHK, fromInputHK, toInputHK, toSliderHK);
-
 /////// Script starts here ///////
+
 getPartialView();
 
 // Closes the filter sidebar if the site is opened on a screen smaller than 700px.
